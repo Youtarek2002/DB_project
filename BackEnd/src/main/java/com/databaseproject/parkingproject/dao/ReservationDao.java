518ff627b7cc360c7bc8b26ab9997e2591d1f0b2
habibatarek26 <147947993+habibatarek26@users.noreply.github.com>
2024-12-25 02:26:29 +0200

    .

--- BackEnd/src/main/java/com/databaseproject/parkingproject/dao/ReservationDao.java
+++ BackEnd/src/main/java/com/databaseproject/parkingproject/dao/ReservationDao.java
@@ -6,29 +6,36 @@ import com.databaseproject.parkingproject.entity.Reservations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
 @Repository
 public class ReservationDao {
-    private static final String SQL_CHECK_SPOT_AVAILABILITY = "SELECT COUNT(*) FROM reservations WHERE parking_spot_id = ? AND start_time < ? AND end_time > ?;";
+    private static final String SQL_GET_RESERVATION_BY_ID = "SELECT id, penalty, start_time, end_time, duration, user_id, parking_spot_id, transaction_id FROM reservations WHERE id = ?";
+    private static final String SQL_UPDATE_TIME_SLOT = "UPDATE time_slots SET status = ? WHERE parking_spot_id = ? AND start_time BETWEEN ? AND ?";
+    private static final String SQL_INSERT_TIME_SLOT = "INSERT INTO time_slots (parking_spot_id, start_time, end_time, status) VALUES (?, ?, ?, ?)";
     private static final String SQL_INSERT_RESERVATION = "INSERT INTO reservations (parking_spot_id, user_id, start_time, end_time, penalty) VALUES (?, ?, ?, ?, ?);";
     private static final String SQL_DELETE_RESERVATION = "DELETE FROM reservations WHERE id = ?;";
     private static final String SQL_UPDATE_RESERVATION = "UPDATE reservations SET parking_spot_id = ?, user_id = ?, start_time = ?, end_time = ?, penalty = ? WHERE id = ?;";
     private static final String SQL_GET_ALL_RESERVATIONS = "SELECT id, penalty, start_time, end_time, duration, user_id, parking_spot_id, transaction_id FROM reservations;";
     private static final String SQL_GET_USER_RESERVATIONS = "SELECT * FROM reservations WHERE user_id = ?;";
-    private static final String SQL_GET_PARKING_LOT_RESERVATIONS = "SELECT r.* FROM reservations r JOIN parking_spots ps ON r.parking_spot_id = ps.id WHERE ps.parking_lot_id = ?;";
     private static final String SQL_MARK_EXPIRED_RESERVATIONS = "UPDATE reservations SET penalty = penalty + 50 WHERE end_time < NOW() AND penalty = 0;";
-    private static final String SQL_GET_AVAILABLE_SPOTS = "SELECT * FROM parking_spots ps " +
+    private static final String SQL_GET_AVAILABLE_SPOTS = "SELECT ps.id, ps.type, ps.price, ps.parking_lot_id " +
+            "FROM parking_spots ps " +
             "WHERE ps.parking_lot_id = ? " +
-            "AND ps.id NOT IN (SELECT rs.parking_spot_id FROM reservations rs " +
-            "WHERE rs.start_time < ? AND rs.end_time > ?)";
+            "AND ps.id NOT IN (" +
+            "    SELECT ts.parking_spot_id " +
+            "    FROM time_slots ts " +
+            "    WHERE ts.start_time BETWEEN ? AND ? " +
+            "    AND ts.status != 'AVAILABLE'" +
+            ");";
     private static final String SQL_USER_PENALTY = "SELECT SUM(penalty) FROM reservations WHERE user_id = ?";
-
+    private static final String SQL_GET_PARKING_LOT_RESERVATIONS = "SELECT r.* FROM reservations r JOIN parking_spots ps ON r.parking_spot_id = ps.id WHERE ps.parking_lot_id = ?;";
+    private static final String SQL_CHECK_SPOT_AVAILABILITY = "SELECT COUNT(*) FROM reservations WHERE parking_spot_id = ? AND start_time < ? AND end_time > ?;";
     private final JdbcTemplate jdbcTemplate;
 
     public ReservationDao(JdbcTemplate jdbcTemplate) {
@@ -36,32 +43,43 @@ public class ReservationDao {
     }
 
     public ResponseMessageDto createReservation(Reservations reservation) {
+        if (isSpotAlreadyBooked(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime())) {
+            return new ResponseMessageDto("Parking spot already reserved for the selected time.", false, 400, null);
+        }
+
         int rowsAffected = jdbcTemplate.update(SQL_INSERT_RESERVATION,
                 reservation.getParkingSpotId(),
                 reservation.getUserId(),
                 reservation.getStartTime(),
                 reservation.getEndTime(),
                 reservation.getPenalty());
-        return new ResponseMessageDto(
-                rowsAffected == 1 ? "Reservation created successfully" : "Error creating reservation",
-                rowsAffected == 1,
-                rowsAffected == 1 ? 200 : 500,
-                null);
+
+        if (rowsAffected == 1) {
+            createTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "RESERVED");
+            return new ResponseMessageDto("Reservation created successfully", true, 200, null);
+        } else {
+            return new ResponseMessageDto("Error creating reservation", false, 500, null);
+        }
     }
 
     public ResponseMessageDto deleteReservation(int id) {
-        int rowsAffected = jdbcTemplate.update(SQL_DELETE_RESERVATION, id);
-        return new ResponseMessageDto(
-                rowsAffected == 1 ? "Reservation deleted successfully" : "Error deleting reservation",
-                rowsAffected == 1,
-                rowsAffected == 1 ? 200 : 500,
-                null);
+        Reservations reservation = getReservationById(id);
+        if (reservation != null) {
+            int rowsAffected = jdbcTemplate.update(SQL_DELETE_RESERVATION, id);
+
+            if (rowsAffected == 1) {
+                updateTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "AVAILABLE");
+                return new ResponseMessageDto("Reservation deleted successfully", true, 200, null);
+            }
+        }
+        return new ResponseMessageDto("Error deleting reservation", false, 500, null);
     }
 
     public ResponseMessageDto updateReservation(Reservations reservation) {
         if (isSpotAlreadyBooked(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime())) {
             return new ResponseMessageDto("Parking spot already reserved for the selected time.", false, 400, null);
         }
+
         int rowsAffected = jdbcTemplate.update(SQL_UPDATE_RESERVATION,
                 reservation.getParkingSpotId(),
                 reservation.getUserId(),
@@ -69,26 +87,14 @@ public class ReservationDao {
                 reservation.getEndTime(),
                 reservation.getPenalty(),
                 reservation.getId());
-        return new ResponseMessageDto(
-                rowsAffected == 1 ? "Reservation updated successfully" : "Error updating reservation",
-                rowsAffected == 1,
-                rowsAffected == 1 ? 200 : 500,
-                null);
+
+        if (rowsAffected == 1) {
+            updateTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "RESERVED");
+            return new ResponseMessageDto("Reservation updated successfully", true, 200, null);
+        } else {
+            return new ResponseMessageDto("Error updating reservation", false, 500, null);
+        }
     }
-
-    public List<Reservations> getAllReservations() {
-        return jdbcTemplate.query(SQL_GET_ALL_RESERVATIONS, new ReservationRowMapper());
-    }
-
-    public List<ParkingSpots> getAvailableSpots(String startTime, String endTime, int lotId) {
-        return jdbcTemplate.query(SQL_GET_AVAILABLE_SPOTS, new Object[]{lotId, endTime, startTime}, new ParkingSpotRowMapper());
-    }
-
-
-    public List<Reservations> getUserReservations(int userId) {
-        return jdbcTemplate.query(SQL_GET_USER_RESERVATIONS, new Object[]{userId}, new ReservationRowMapper());
-    }
-
     public List<Reservations> getParkingLotReservations(int parkingLotId) {
         return jdbcTemplate.query(SQL_GET_PARKING_LOT_RESERVATIONS, new Object[]{parkingLotId}, new ReservationRowMapper());
     }
@@ -97,6 +103,19 @@ public class ReservationDao {
         Integer count = jdbcTemplate.queryForObject(SQL_CHECK_SPOT_AVAILABILITY, new Object[]{parkingSpotId, endTime, startTime}, Integer.class);
         return count != null && count > 0;
     }
+
+    public List<Reservations> getAllReservations() {
+        return jdbcTemplate.query(SQL_GET_ALL_RESERVATIONS, new ReservationRowMapper());
+    }
+
+    public List<ParkingSpots> getAvailableSpots(String startTime, String endTime, int lotId) {
+        return jdbcTemplate.query(SQL_GET_AVAILABLE_SPOTS, new Object[]{lotId, startTime, endTime}, new ParkingSpotRowMapper());
+    }
+
+    public List<Reservations> getUserReservations(int userId) {
+        return jdbcTemplate.query(SQL_GET_USER_RESERVATIONS, new Object[]{userId}, new ReservationRowMapper());
+    }
+
     public ResponseMessageDto expireReservations() {
         int rowsAffected = jdbcTemplate.update(SQL_MARK_EXPIRED_RESERVATIONS);
         return new ResponseMessageDto(
@@ -106,57 +125,53 @@ public class ReservationDao {
                 null);
     }
 
-    public class ReservationRowMapper implements RowMapper<Reservations> {
-
-        @Override
-        public Reservations mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Reservations reservation = new Reservations();
-            reservation.setId(rs.getInt("id"));
-
-            int penalty = rs.getInt("penalty");
-            if (rs.wasNull()) {
-                penalty = 0;
-            }
-            reservation.setPenalty(penalty);
-
-            reservation.setStartTime(rs.getTimestamp("start_time").toLocalDateTime());
-            reservation.setEndTime(rs.getTimestamp("end_time").toLocalDateTime());
-
-            String durationStr = rs.getString("duration");
-            if (durationStr != null && !durationStr.isEmpty()) {
-                try {
-                    reservation.setDuration(Duration.parse(durationStr));
-                } catch (Exception e) {
-                    reservation.setDuration(Duration.ZERO);
-                }
-            } else {
-                reservation.setDuration(Duration.ZERO);
-            }
-
-            reservation.setUserId(rs.getInt("user_id"));
-            reservation.setParkingSpotId(rs.getInt("parking_spot_id"));
-            reservation.setTransactionId(rs.getInt("transaction_id"));
-
-            return reservation;
-        }
-    }
-
-
-    public class ParkingSpotRowMapper implements RowMapper<ParkingSpots> {
-        @Override
-        public ParkingSpots mapRow(ResultSet rs, int rowNum) throws SQLException {
-            ParkingSpots parkingSpot = new ParkingSpots();
-            parkingSpot.setId(rs.getInt("id"));
-            parkingSpot.setType(rs.getString("type"));
-            parkingSpot.setPrice(rs.getInt("price"));
-            parkingSpot.setParkingLotId(rs.getInt("parking_lot_id"));
-            return parkingSpot;
-        }
-    }
     public int getTotalPenaltyForUser(int userId) {
         Integer totalPenalty = jdbcTemplate.queryForObject(SQL_USER_PENALTY, new Object[]{userId}, Integer.class);
         return totalPenalty != null ? totalPenalty : 0;
     }
 
+    private void createTimeSlotsForReservation(int parkingSpotId, LocalDateTime startTime, LocalDateTime endTime, String status) {
+        LocalDateTime currentTime = startTime;
+        while (currentTime.isBefore(endTime) || currentTime.equals(endTime)) {
+            LocalDateTime nextTime = currentTime.plusMinutes(60);
+            jdbcTemplate.update(SQL_INSERT_TIME_SLOT, parkingSpotId, currentTime, nextTime, status);
+            currentTime = nextTime;
+        }
+    }
 
+    private void updateTimeSlotsForReservation(int parkingSpotId, LocalDateTime startTime, LocalDateTime endTime, String status) {
+        jdbcTemplate.update(SQL_UPDATE_TIME_SLOT, status, parkingSpotId, startTime, endTime);
+    }
+
+    private Reservations getReservationById(int id) {
+        return jdbcTemplate.queryForObject(SQL_GET_RESERVATION_BY_ID, new Object[]{id}, new ReservationRowMapper());
+    }
+
+    public class ReservationRowMapper implements RowMapper<Reservations> {
+        @Override
+        public Reservations mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Reservations reservation = new Reservations();
+            reservation.setId(rs.getInt("id"));
+            reservation.setPenalty(rs.getInt("penalty"));
+            reservation.setStartTime(rs.getTimestamp("start_time").toLocalDateTime());
+            reservation.setEndTime(rs.getTimestamp("end_time").toLocalDateTime());
+            reservation.setDuration(rs.getObject("duration", Duration.class));
+            reservation.setUserId(rs.getInt("user_id"));
+            reservation.setParkingSpotId(rs.getInt("parking_spot_id"));
+            reservation.setTransactionId(rs.getInt("transaction_id"));
+            return reservation;
+        }
+    }
+
+    public class ParkingSpotRowMapper implements RowMapper<ParkingSpots> {
+        @Override
+        public ParkingSpots mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ParkingSpots spot = new ParkingSpots();
+            spot.setId(rs.getInt("id"));
+            spot.setType(rs.getString("type"));
+            spot.setPrice(rs.getInt("price"));
+            spot.setParkingLotId(rs.getInt("parking_lot_id"));
+            return spot;
+        }
+    }
 }
--- BackEnd/src/main/resources/schema.sql
+++ BackEnd/src/main/resources/schema.sql
@@ -1,86 +1,179 @@
+package com.databaseproject.parkingproject.dao;
 
-create  table IF NOT EXISTS users(
-id  int auto_increment primary Key not null,
-fname  varchar(100),
-lname	varchar(100),
-username	varchar(100) unique not null,
-email	varchar(200) unique not null,
-phone	varchar(100),
-password	varchar(100) not null,
-payment_method	varchar(100),
-role	enum('DRIVER', 'PARKING_LOT_MANAGER', 'SYSTEM_ADMIN') not null,
-license_plate	varchar(100));
+import com.databaseproject.parkingproject.dto.ResponseMessageDto;
+import com.databaseproject.parkingproject.entity.ParkingSpots;
+import com.databaseproject.parkingproject.entity.Reservations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
-create table IF NOT EXISTS notifications(
-id int auto_increment primary Key not null,
-status	enum('PENDING','SENT','FAILED','DELIVERED') not null,
-time	time,
-message	varchar(500),
-user_id	int,
-foreign key (user_id) references users(id) on delete cascade
-);
+@Repository
+public class ReservationDao {
 
-create table IF NOT EXISTS transactions(
-id	int auto_increment primary key not null,
-amount	int,
-time	time);
+    private static final String SQL_INSERT_RESERVATION = "INSERT INTO reservations (parking_spot_id, user_id, start_time, end_time, penalty) VALUES (?, ?, ?, ?, ?);";
+    private static final String SQL_DELETE_RESERVATION = "DELETE FROM reservations WHERE id = ?;";
+    private static final String SQL_UPDATE_RESERVATION = "UPDATE reservations SET parking_spot_id = ?, user_id = ?, start_time = ?, end_time = ?, penalty = ? WHERE id = ?;";
+    private static final String SQL_GET_ALL_RESERVATIONS = "SELECT id, penalty, start_time, end_time, duration, user_id, parking_spot_id, transaction_id FROM reservations;";
+    private static final String SQL_GET_USER_RESERVATIONS = "SELECT * FROM reservations WHERE user_id = ?;";
+    private static final String SQL_MARK_EXPIRED_RESERVATIONS = "UPDATE reservations SET penalty = penalty + 50 WHERE end_time < NOW() AND penalty = 0;";
+    private static final String SQL_GET_AVAILABLE_SPOTS = "SELECT ps.id, ps.type, ps.price, ps.parking_lot_id " +
+            "FROM parking_spots ps " +
+            "WHERE ps.parking_lot_id = ? " +
+            "AND ps.id NOT IN (" +
+            "    SELECT ts.parking_spot_id " +
+            "    FROM time_slots ts " +
+            "    WHERE ts.start_time BETWEEN ? AND ? " +
+            "    AND ts.status != 'AVAILABLE'" +
+            ");";
+    private static final String SQL_USER_PENALTY = "SELECT SUM(penalty) FROM reservations WHERE user_id = ?";
 
+    private final JdbcTemplate jdbcTemplate;
 
-CREATE TABLE IF NOT EXISTS parking_lots (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    disabled_count INT NOT NULL DEFAULT 0,
-    regular_count INT NOT NULL DEFAULT 0,
-    EV_count INT NOT NULL DEFAULT 0,
-    location VARCHAR(500) NOT NULL,
-    parking_lot_manager INT,
-    admitted BOOLEAN,
-    FOREIGN KEY (parking_lot_manager) REFERENCES users(id)
-);
+    public ReservationDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-create table IF NOT EXISTS parking_spots(
-id int auto_increment primary key not null,
-type	varchar(100),
-price	int DEFAULT 0,
-parking_lot_id	int,
-foreign key (parking_lot_id) references parking_lots(id) on delete cascade
- );
- create table IF NOT EXISTS time_slots(
- id int auto_increment primary key not null,
- status	enum('OCCUPIED','AVAILABLE','RESERVED') not null DEFAULT "AVAILABLE",
- parking_spot_id int,
- start_time	time,
- foreign key (parking_spot_id) references parking_spots(id) on delete cascade
-  );
+    public ResponseMessageDto createReservation(Reservations reservation) {
+        // Insert the reservation into the reservations table
+        int rowsAffected = jdbcTemplate.update(SQL_INSERT_RESERVATION,
+                reservation.getParkingSpotId(),
+                reservation.getUserId(),
+                reservation.getStartTime(),
+                reservation.getEndTime(),
+                reservation.getPenalty());
 
-create table IF NOT EXISTS reports(
-id int auto_increment primary key not null,
-occupancy int,
-violation	varchar(100),
-revenue		int,
-parking_spot_id	int,
-foreign key (parking_spot_id) references parking_spots(id) on delete cascade
-);
+        if (rowsAffected == 1) {
+            // Update the time_slots table for the parking spot during the reservation period
+            createTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "RESERVED");
+            return new ResponseMessageDto("Reservation created successfully", true, 200, null);
+        } else {
+            return new ResponseMessageDto("Error creating reservation", false, 500, null);
+        }
+    }
 
-CREATE TABLE IF NOT EXISTS reservations (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    penalty INT DEFAULT 0,  -- Set default value to 0
-    start_time DATETIME,
-    end_time DATETIME,
-    duration TIME,
-    user_id INT,
-    parking_spot_id INT,
-    transaction_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (parking_spot_id) REFERENCES parking_spots(id),
-    FOREIGN KEY (transaction_id) REFERENCES transactions(id) ON DELETE CASCADE
-);
+    public ResponseMessageDto deleteReservation(int id) {
+        // Retrieve the reservation details to delete corresponding time slots
+        Reservations reservation = getReservationById(id);
+        if (reservation != null) {
+            // Delete the reservation from the reservations table
+            int rowsAffected = jdbcTemplate.update(SQL_DELETE_RESERVATION, id);
 
+            if (rowsAffected == 1) {
+                // Reset time slots status to AVAILABLE after reservation is deleted
+                updateTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "AVAILABLE");
+                return new ResponseMessageDto("Reservation deleted successfully", true, 200, null);
+            }
+        }
+        return new ResponseMessageDto("Error deleting reservation", false, 500, null);
+    }
 
-CREATE TABLE IF NOT EXISTS tokens (
-    id int auto_increment primary key not null,
-    token VARCHAR(255) NOT NULL UNIQUE,
-    revoked BOOLEAN NOT NULL DEFAULT FALSE,
-    user_id int,
-    foreign key (user_id) references users(id)
-);
+    public ResponseMessageDto updateReservation(Reservations reservation) {
+        // First, check if the parking spot is available
+        if (isSpotAlreadyBooked(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime())) {
+            return new ResponseMessageDto("Parking spot already reserved for the selected time.", false, 400, null);
+        }
+
+        // Update reservation details in the database
+        int rowsAffected = jdbcTemplate.update(SQL_UPDATE_RESERVATION,
+                reservation.getParkingSpotId(),
+                reservation.getUserId(),
+                reservation.getStartTime(),
+                reservation.getEndTime(),
+                reservation.getPenalty(),
+                reservation.getId());
+
+        if (rowsAffected == 1) {
+            // Update time slots status to reflect the new reservation
+            updateTimeSlotsForReservation(reservation.getParkingSpotId(), reservation.getStartTime(), reservation.getEndTime(), "RESERVED");
+            return new ResponseMessageDto("Reservation updated successfully", true, 200, null);
+        } else {
+            return new ResponseMessageDto("Error updating reservation", false, 500, null);
+        }
+    }
+
+    public List<Reservations> getAllReservations() {
+        return jdbcTemplate.query(SQL_GET_ALL_RESERVATIONS, new ReservationRowMapper());
+    }
+
+    public List<ParkingSpots> getAvailableSpots(String startTime, String endTime, int lotId) {
+        return jdbcTemplate.query(SQL_GET_AVAILABLE_SPOTS, new Object[]{lotId, startTime, endTime}, new ParkingSpotRowMapper());
+    }
+
+    public List<Reservations> getUserReservations(int userId) {
+        return jdbcTemplate.query(SQL_GET_USER_RESERVATIONS, new Object[]{userId}, new ReservationRowMapper());
+    }
+
+    public ResponseMessageDto expireReservations() {
+        int rowsAffected = jdbcTemplate.update(SQL_MARK_EXPIRED_RESERVATIONS);
+        return new ResponseMessageDto(
+                rowsAffected > 0 ? "Expired reservations updated successfully" : "No reservations to expire",
+                rowsAffected > 0,
+                rowsAffected > 0 ? 200 : 204,
+                null);
+    }
+
+    public int getTotalPenaltyForUser(int userId) {
+        Integer totalPenalty = jdbcTemplate.queryForObject(SQL_USER_PENALTY, new Object[]{userId}, Integer.class);
+        return totalPenalty != null ? totalPenalty : 0;
+    }
+
+    private boolean isSpotAlreadyBooked(int parkingSpotId, LocalDateTime startTime, LocalDateTime endTime) {
+        String sql = "SELECT COUNT(*) FROM reservations WHERE parking_spot_id = ? " +
+                "AND start_time < ? AND end_time > ?";
+        Integer count = jdbcTemplate.queryForObject(sql, new Object[]{parkingSpotId, endTime, startTime}, Integer.class);
+        return count != null && count > 0;
+    }
+
+    private void createTimeSlotsForReservation(int parkingSpotId, LocalDateTime startTime, LocalDateTime endTime, String status) {
+        String insertTimeSlot = "INSERT INTO time_slots (parking_spot_id, start_time, status) VALUES (?, ?, ?)";
+        LocalDateTime currentTime = startTime;
+        while (currentTime.isBefore(endTime) || currentTime.equals(endTime)) {
+            jdbcTemplate.update(insertTimeSlot, parkingSpotId, currentTime, status);
+            currentTime = currentTime.plusMinutes(30); // Assuming slots are 30 minutes, adjust as necessary
+        }
+    }
+
+    private void updateTimeSlotsForReservation(int parkingSpotId, LocalDateTime startTime, LocalDateTime endTime, String status) {
+        String updateTimeSlot = "UPDATE time_slots SET status = ? WHERE parking_spot_id = ? AND start_time BETWEEN ? AND ?";
+        jdbcTemplate.update(updateTimeSlot, status, parkingSpotId, startTime, endTime);
+    }
+
+    private Reservations getReservationById(int id) {
+        String sql = "SELECT id, penalty, start_time, end_time, duration, user_id, parking_spot_id, transaction_id FROM reservations WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new ReservationRowMapper());
+    }
+
+    public class ReservationRowMapper implements RowMapper<Reservations> {
+        @Override
+        public Reservations mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Reservations reservation = new Reservations();
+            reservation.setId(rs.getInt("id"));
+            reservation.setPenalty(rs.getInt("penalty"));
+            reservation.setStartTime(rs.getTimestamp("start_time").toLocalDateTime());
+            reservation.setEndTime(rs.getTimestamp("end_time").toLocalDateTime());
+            reservation.setDuration(rs.getObject("duration", Duration.class));
+            reservation.setUserId(rs.getInt("user_id"));
+            reservation.setParkingSpotId(rs.getInt("parking_spot_id"));
+            reservation.setTransactionId(rs.getInt("transaction_id"));
+            return reservation;
+        }
+    }
+
+    public class ParkingSpotRowMapper implements RowMapper<ParkingSpots> {
+        @Override
+        public ParkingSpots mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ParkingSpots parkingSpot = new ParkingSpots();
+            parkingSpot.setId(rs.getInt("id"));
+            parkingSpot.setType(rs.getString("type"));
+            parkingSpot.setPrice(rs.getInt("price"));
+            parkingSpot.setParkingLotId(rs.getInt("parking_lot_id"));
+            return parkingSpot;
+        }
+    }
+}
